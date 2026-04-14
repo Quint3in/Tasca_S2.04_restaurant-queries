9773fe28520a793712422d40b557d289411299c8
Joel-Quintana <joelqn95@gmail.com>
2026-04-14 10:33:35 +0200

    Add query for restaurant details including borough and cuisine
    Added query to retrieve restaurant_id, name, borough, and cuisine.

--- query/queries.js
+++ query/queries.js
@@ -5,6 +5,7 @@ db.restaurants.find({},{_id:0})
 db.restaurants.find({},{_id:0, restaurant_id:1, name:1})
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
+db.restaurants.find({},{_id:0, restaurant_id:1, name:1, borough:1, cuisine:1})
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
 
